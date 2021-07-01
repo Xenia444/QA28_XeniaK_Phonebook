b2af04729b32bfc54bb27feae3346e7b63cc0921
Xenia444 <86400654+Xenia444@users.noreply.github.com>
2021-07-01 15:19:16 +0300

    New project

---
+++ src/test/java/HomeTest.java
@@ -0,0 +1,34 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class HomeTest {
+
+    WebDriver wd;
+
+    @BeforeMethod
+    public void openChrome() {
+        wd = new ChromeDriver();
+        wd.navigate().to("https://contacts-app.tobbymarshall815.vercel.app/home");
+    }
+
+    @Test
+    public void AboutTest() {
+        List<WebElement> list = wd.findElements(By.tagName("a"));
+        WebElement element = list.get(1);
+        element.click();
+        WebElement element2 = list.get(0);
+        element2.click();
+    }
+
+    @AfterMethod
+    public void closeChrome() {
+        wd.quit();
+    }
+}
